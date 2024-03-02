bfaa87fcd832bd5fec93829009c54b99fad35615
ArJuN-888 <ksarjun635@gmail.com>
2024-03-02 12:02:44 +0530

    updated

--- Server/Config/Db.js
+++ Server/Config/Db.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose")
-const DB = mongoose.connect("mongodb://127.0.0.1:27017/Project")
-.then(()=>console.log("Database connected"))
-.catch((error)=>console.log("Unable to establish a connection",error))
-module.exports = DB
+const mongoose = require("mongoose");
+const DB = mongoose
+  .connect("mongodb://127.0.0.1:27017/Project")
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.log("Unable to establish a connection", error));
+module.exports = DB;
